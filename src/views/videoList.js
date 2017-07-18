07b2a558500920ac27cb48aaa7c9ad25ab113d59
Hack Reactor Student <student@hackreactor.com>
2017-07-18 12:30:17 -0700

    Added event listeners to select video and handle sync trigger

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,5 +1,9 @@
 var VideoListView = Backbone.View.extend({
   
+  initialize: function() {
+    this.collection.on('sync', this.render, this);
+  },
+  
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,16 +1,21 @@
 var VideoListEntryView = Backbone.View.extend({
   
-  // el: '.video-list',
   
   initialize: function() {
     this.model.on('change', this.render, this);
   },
   
+  events: {
+    'click .video-list-entry-title': 'handleClick'
+  },
+  
+  handleClick: function() {
+    this.model.select();
+  },  
+  
   render: function() {
-    // this.model.attributes 
 
     this.$el.html(this.template(this.model.attributes));
-    console.log(this.model.attributes);
     return this.$el;
   },
 
